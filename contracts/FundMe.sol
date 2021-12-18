c1501052226a0faf48eed63c4013a9029e6848d9
Uwa <uwa@valapp.io>
2021-12-18 05:45:42 +0000

    my first blockchain push

---
+++ contracts/FundMe.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // $50
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more eth");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        //AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        //AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,)
+            = priceFeed.latestRoundData();
+
+        return uint256(answer * 10 ** 10); // answer is 8 decimals by default
+        // use the decimals() function to get the number of decimals in the value
+        // best practice is to put everything in the same denomination --> WEI or 10 ** 18
+        // 3,994.38368888
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / (10 ** 18);
+        return ethAmountInUsd;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        // minimumUSD
+        uint256 minimumUSD = 50 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (minimumUSD * precision) / price;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "OLE!!");
+        _; // run the rest of the code.
+    }
+
+    function withdraw() payable onlyOwner public {
+        payable(msg.sender).transfer(address(this).balance);
+        for (uint256 funderIndex=0; funderIndex<funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset funders array
+        funders = new address[](0);
+    }
+}
